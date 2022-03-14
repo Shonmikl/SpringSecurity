6ad3519f390c27e46ec66db360d10d4d439fc333
shonmikl <mikhailegorov676@gmail.com>
2022-03-14 21:14:09 +0300

    Initial commit

---
+++ src/main/java/com/mikhailegorov/spring/security/configuration/MySecurityInit.java
@@ -0,0 +1,6 @@
+package com.mikhailegorov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
